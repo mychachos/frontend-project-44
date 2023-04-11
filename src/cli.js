5906ab90dafc42f7de7fc30fc6bdf10b70fae2d7
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-11 07:17:52 +0300

    git commit -m 'add export'

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
+export const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
