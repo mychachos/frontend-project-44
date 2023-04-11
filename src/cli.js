355e18254954d88e6987d71a15c9e4add595bd1c
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-11 08:06:49 +0300

    add file with func that is ask a name

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
 import readlineSync from 'readline-sync';
 
-export const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+export const userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
