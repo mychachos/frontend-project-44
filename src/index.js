28133487bcd0325f8603cd351b1b165157589ef5
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-18 13:45:56 +0300

    add logic func for games

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const roundCount = 3;
+
+const gameLogic = (task, game) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(task);
+
+  for (let i = 0; i < roundCount; i += 1) {
+    const [expression, expectedAnswer] = game();
+    console.log(`Question: ${expression}`);
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === expectedAnswer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`${answer} is wrong answer ;(. Correct answer was ${expectedAnswer}.\nLet's try again, ${name}!`);
+    }
+  }
+  return console.log(`Congratulations, ${name}!`);
+};
+export default gameLogic;
