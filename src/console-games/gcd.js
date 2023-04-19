86ebed3e4d8cf6c5c17d8de51b4b478883871517
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-19 08:10:18 +0300

    add gcd func

---
+++ src/console-games/gcd.js
@@ -0,0 +1,28 @@
+/* eslint-disable operator-assignment */
+import getRandomNumber from '../randomNum.js';
+import gameLogic from '../index.js';
+
+const nod = (num1, num2) => {
+  while (num1 !== 0 && num2 !== 0) {
+    if (Math.abs(num1) > (Math.abs(num2))) {
+      num1 = num1 % num2;
+    } else {
+      num2 = num2 % num1;
+    }
+  }
+  return num1 + num2;
+};
+
+const gcd = () => {
+  const firstNum = getRandomNumber();
+  const secondNum = getRandomNumber();
+  const taskNumbers = `${firstNum} ${secondNum}`;
+  const expectedAnswer = nod(firstNum, secondNum);
+
+  return [taskNumbers, String(expectedAnswer)];
+};
+
+export default () => {
+  const taskOfGcd = 'Find the greatest common divisor of given numbers.';
+  return gameLogic(taskOfGcd, gcd);
+};
