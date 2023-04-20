6450c7eb99ee7a6403ec5a663469616d8a97338b
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-20 19:26:21 +0300

    realize progression game

---
+++ src/console-games/progression.js
@@ -0,0 +1,33 @@
+import gameLogic from '../index.js';
+import getRandomNumber from '../randomNum.js';
+
+const getProgression = () => {
+  const getNumbers = () => {
+    const startProgression = getRandomNumber();
+    const randomInterval = Math.round(Math.random() * 3) + 2;
+    const result = [];
+    for (let i = startProgression; result.length < 10; i += randomInterval) {
+      result.push(i);
+    }
+    return result;
+  };
+
+  const numbers = getNumbers();
+  const randomIndex = Math.round(Math.random() * 9);
+  const expectedAnswer = numbers[randomIndex];
+
+  const expression = () => {
+    if (numbers.includes(numbers[randomIndex])) {
+      numbers[randomIndex] = '..';
+    }
+    return numbers.join(' ');
+  };
+
+  return [expression(), String(expectedAnswer)];
+};
+
+export default () => {
+  const taskOfProgression = 'What number is missing in the progression?';
+
+  return gameLogic(taskOfProgression, getProgression);
+};
