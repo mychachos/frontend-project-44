9f4db97b293be03c4625bcd3b6b79919cd4224e1
Vladimir Vasiliev <endnephew@gmail.com>
2023-04-22 15:10:30 +0300

    fix err

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,18 +4,17 @@
 import getRandomNumber from '../randomNum.js';
 import gameLogic from '../index.js';
 
-const commonDivider = (num1, num2) => {
-  while (num1 !== 0 && num2 !== 0) {
-    if (Math.abs(num1) > (Math.abs(num2))) {
-      num1 = num1 % num2;
-    } else {
-      num2 = num2 % num1;
-    }
-  }
-  return num1 + num2;
-};
-
 const gcdGame = () => {
+  const commonDivider = (num1, num2) => {
+    while (num1 !== 0 && num2 !== 0) {
+      if (Math.abs(num1) > (Math.abs(num2))) {
+        num1 = num1 % num2;
+      } else {
+        num2 = num2 % num1;
+      }
+    }
+    return num1 + num2;
+  };
   const firstNum = getRandomNumber();
   const secondNum = getRandomNumber();
   const taskNumbers = `${firstNum} ${secondNum}`;
