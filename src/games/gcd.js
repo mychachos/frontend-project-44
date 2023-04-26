820cedd87166c7f8587d7a7510552fb1b8cd09d7
Vladimir <107367865+mychachos@users.noreply.github.com>
2023-04-26 15:14:24 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,9 +4,9 @@ import gameLogic from '../index.js';
 const commonDivider = (num1, num2) => {
   while (num1 !== 0 && num2 !== 0) {
     if (Math.abs(num1) > (Math.abs(num2))) {
-      num1 = num1 % num2;
+      num1 %= num2;
     } else {
-      num2 = num2 % num1;
+      num2 %= num1;
     }
   }
   return num1 + num2;
