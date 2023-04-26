c7fcf3277dd4df8408ebc8a44ae8606dc6a03406
Vladimir <107367865+mychachos@users.noreply.github.com>
2023-04-26 15:31:23 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import gameLogic from '../index.js';
 
 const isPrimeNumber = (num) => {
   if (num < 2) return false;
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
